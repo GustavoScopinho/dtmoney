7e6ff18a5361e2cc537b28de49befa4f93b31a16
Gustavo Scopinho <102439841+GustavoScopinho@users.noreply.github.com>
2022-08-25 16:50:38 -0300

    First commit

---
+++ dtmoneyfirst/src/App.tsx
@@ -0,0 +1,15 @@
+import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header"
+
+
+export function App() {
+
+  return (
+    <>
+      <Header/>
+      <GlobalStyle/>
+     
+    </>
+  )
+}
+
---
+++ dtmoneyfirst/src/styles/global.ts
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #202024;
+    --red: #e52e4d;
+    --black: #000;
+    --text-title: #E1E1E6;
+  }
+
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html {
+  @media (max-width: 1080px) {
+    font-size: 93.75%;
+  }
+
+  @media (max-width: 720px) {
+    font-size: 87.5%;
+  }
+}
+
+body {
+  background: var(--background);
+  -webkit-font-smoothing: antialiased;
+}
+
+body, input, textarea, button {
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+}
+
+h1, h2, h3, h4, h5, h6, strong {
+  font-weight: 700;
+}
+
+button {
+  cursor: pointer;
+}
+
+[disabled] {
+  opacity: 0.6;
+  cursor: not-allowed;
+}
+
+body {
+  background: var(--background);
+  -webkit-font-smoothing: antialiased;
+}
+
+`
