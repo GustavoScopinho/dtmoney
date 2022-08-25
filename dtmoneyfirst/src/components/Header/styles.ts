973db8181d2338be0ab9ec40d5af59ad92325af2
Gustavo Scopinho <102439841+GustavoScopinho@users.noreply.github.com>
2022-08-25 17:12:48 -0300

    Header

--- dtmoneyfirst/src/components/Header/styles.ts
+++ dtmoneyfirst/src/components/Header/styles.ts
@@ -7,4 +7,25 @@ background: var(--black);
 export const Content = styled.div`
   max-width: 1120px;
   margin: 0 auto;
+
+  padding: 2rem  1rem 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    font-size: 1rem;
+    background-color: #00875F;
+    color: #FFF;
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
 `;
