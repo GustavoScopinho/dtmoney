a32766be7d61ae736f9736f838fdb2331a8645e0
Gustavo Scopinho <102439841+GustavoScopinho@users.noreply.github.com>
2022-09-08 18:31:50 -0300

    Summary calculation

--- dtmoneyfirst/src/TransactionsContext.tsx
+++ dtmoneyfirst/src/TransactionsContext.tsx
@@ -11,12 +11,21 @@ interface Transaction {
   createdAt: string;
 }
 
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
 interface TransactionProviderProps {
   children: ReactNode;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
+}
 
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData 
+);
 
 
 export function TransactionsProvider ({children}: TransactionProviderProps) {
@@ -28,8 +37,21 @@ export function TransactionsProvider ({children}: TransactionProviderProps) {
     .then(response => setTransactions(response.data.transactions))
   }, []);
 
+ async function createTransaction(transactionInput: TransactionInput) {
+  const response = await api.post('/transactions', {
+    ...transactionInput,
+    createdAt: new Date ()
+  })
+  const { transaction} = response.data;
+
+  setTransactions([
+    ...transactions,
+    transaction,
+  ])
+  }
+
   return(
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={{transactions, createTransaction}}>
       {children}
 
     </TransactionsContext.Provider>
--- dtmoneyfirst/src/components/Summary/index.tsx
+++ dtmoneyfirst/src/components/Summary/index.tsx
@@ -8,8 +8,33 @@ import React, { useContext } from 'react'
 
 export function Summary() {
 
-  const transactions = useContext(TransactionsContext);
-  console.log(transactions);
+  const {transactions} = useContext(TransactionsContext);
+
+  // const totalDeposits = transactions.reduce((acc, transaction)=>{
+  //   if (transaction.type === 'deposit' ) {
+  //     return acc + transaction.amount;
+  //   }
+
+  //   return acc;
+  // }, 0)
+
+  const summary = transactions.reduce((acc, transaction) => {
+
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    }else {
+      acc.withdraws += transaction.amount;
+      acc.total -= transaction.amount;
+
+    }
+    return acc;
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0,
+
+  })
 
   return(
       <Container>
@@ -18,7 +43,13 @@ export function Summary() {
             <p>Entradas</p>
             <img src={upImg} alt="Entradas" />
           </header>
-          <strong>R$1000,00</strong>
+          <strong>
+          {new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL' 
+              }).format(summary.deposits)}
+            
+            </strong>
         </div>
 
         <div>
@@ -26,7 +57,12 @@ export function Summary() {
             <p>Saídas</p>
             <img src={downImg} alt="Saidas" />
           </header>
-          <strong>- R$500,00</strong>
+          <strong>
+            - {new Intl.NumberFormat('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL' 
+                      }).format(summary.withdraws)}
+            </strong>
         </div>
 
         <div className="Total">
@@ -34,7 +70,12 @@ export function Summary() {
             <p>Total</p>
             <img src={totalImg} alt="Total" />
           </header>
-          <strong>R$500,00</strong>
+          <strong>
+          {new Intl.NumberFormat('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL' 
+                      }).format(summary.total)}
+            </strong>
         </div>
 
       </Container>
